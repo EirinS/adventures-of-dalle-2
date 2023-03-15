cdb0c8f00eff31ee96753a0f531d872ad8ceae82
Xhonoz <markus.loland@hotmail.com>
2023-03-15 12:16:39 +0100

    remove unused variables

--- src/state/items.ts
+++ src/state/items.ts
@@ -1,20 +1,13 @@
 import { itemHub } from "./rooms";
 
-export let hasCarton = false;
-export let hasKey = false;
-export let hasFlashlight = false;
-
 export function collectKey() {
   itemHub.addItem("key");
-  hasKey = true;
 }
 
 export function collectCarton() {
   itemHub.addItem("carton");
-  hasCarton = true;
 }
 
 export function collectFlashlight() {
   itemHub.addItem("flashlight");
-  hasFlashlight = true;
 }
