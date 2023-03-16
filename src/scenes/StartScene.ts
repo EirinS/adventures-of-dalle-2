f82090bc631375331e238aa1574124529e753e2f
EirinS <eirin.sognnes@outlook.com>
2023-03-16 08:42:27 +0100

    Move start button to bottom of screen

--- src/scenes/StartScene.ts
+++ src/scenes/StartScene.ts
@@ -38,7 +38,7 @@ export class StartScene extends BaseScene {
     const startGame = new Text("START GAME", startStyle);
 
     button.x = Manager.width - startGame.width - 100;
-    button.y = 400;
+    button.y = Manager.height - startGame.height - 100;
     button.beginFill(0xfbe07f);
     button.drawRoundedRect(-24, -6, startGame.width + 48, startGame.height + 12, 20);
     button.endFill();
