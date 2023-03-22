bb7aec3f9072a5aaaffdf53e4557308bd0f90a99
Xhonoz <markus.loland@hotmail.com>
2023-03-22 14:07:14 +0100

    Add text to hints

--- src/scenes/CrystalballScene.ts
+++ src/scenes/CrystalballScene.ts
@@ -1,5 +1,6 @@
 import { Sprite } from "pixi.js";
 import { GameState } from "../components/GameState";
+import { HitBox } from "../components/HitBox";
 import { Direction, NavigationArrow } from "../components/NavigationArrow";
 import { itemHub, livingroom } from "../state/rooms";
 import { BaseScene } from "./BaseScene";
@@ -16,10 +17,19 @@ export class CrystallballScene extends BaseScene {
     ["Diamond", () => GameState.bookRevealed && !GameState.BFound],
   ];
 
+  private hintText: string =
+    "I gaze into the crystal ball and a vision emerges. It appears to be some sort of hint, though I can't quite make out what it means. I'll need to look around the mansion to understand what it means.";
+
   private hintIndex = this.hints.length - 1;
 
   constructor() {
     super(Sprite.from("crystalEmpty"));
+
+    const ball = new HitBox(980, 400, 385);
+    ball.zIndex = 1;
+    this.clickBall = this.clickBall.bind(this);
+    ball.addClickAction(this.clickBall);
+    this.addChild(ball);
   }
 
   public loadNewHint() {
@@ -30,19 +40,28 @@ export class CrystallballScene extends BaseScene {
     let i = (this.hintIndex + 1) % this.hints.length;
     while (i != this.hintIndex) {
       if (this.hints[i][1]()) {
+        this.hintText =
+          "I gaze into the crystal ball and a vision emerges. It appears to be some sort of hint, though I can't quite make out what it means. I'll need to look around the mansion to understand what it means.";
         this.hintIndex = i;
         return this.hints[i][0];
       }
       i = (i + 1) % this.hints.length;
     }
     if (this.hints[i][1]()) {
+      this.hintText =
+        "I gaze into the crystal ball and a vision emerges. It appears to be some sort of hint, though I can't quite make out what it means. I'll need to look around the mansion to understand what it means.";
       this.hintIndex = i;
       return this.hints[i][0];
     }
+    this.hintText = "There don't seem to be any more hints.";
     return "Empty";
   }
 
   public loadNavigation() {
     this.addChild(new NavigationArrow(925, 950, livingroom, Direction.Down));
+  }
+
+  private clickBall() {
+    this.addText([this.hintText]);
   }
 }
